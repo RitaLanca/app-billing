0c9085cf2c8aa4eab72bf58b972f2a31dd1336a6
rita lanca <rita.lanca17@gmail.com>
2025-01-01 17:39:15 +0000

    Add notification context to application

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 import App from './App.tsx'
+import { NotificationProvider } from './context/NotificationContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <NotificationProvider>
         <App />
+    </NotificationProvider>
   </StrictMode>,
 )
